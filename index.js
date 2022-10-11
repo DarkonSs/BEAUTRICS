111ccf746dcfc6d44062447678198f3b257c71aa
designcore <98830257+DarkonSs@users.noreply.github.com>
2022-10-11 17:42:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+// var fs = require('fs');
+
+// fs.appendFile('contact.html', `
+// <!DOCTYPE html>
+// <html lang="en">
+
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+//   <meta property="og:title" content='Beauty and Health - Beautrics'>
+//   <meta property="og:description" content=''>
+//   <meta property="og:image" content="https://media.discordapp.net/attachments/754248467207159849/1020691717986467910/beautricsLogo.png?width=139&height=139">
+//   <meta property="og:type" content="website">
+//   <meta property="og:locale" content="en_US">
+
+//   <link href="CSS/style.css" rel="stylesheet" />
+//   <link href="CSS/header.css" rel="stylesheet" />
+//   <link href="CSS/container.css" rel="stylesheet" />
+//   <link href="CSS/products.css" rel="stylesheet" />
+//   <link href="CSS/footer.css" rel="stylesheet" />
+
+//   <title>Contact - Beautrics</title>
+// </head>
+
+// <body>
+// <!-- UP OF THE PAGE -->
+//   <span id="upOfThePage"></span>
+//   <header id="header"></header>
+//   <script id="headerAnimation" type="text/javascript"></script>
+
+// <footer id="footer"></footer>
+
+// <!-- THE REST -->
+//   <script src="/javaScript/mainHeader.js"></script>
+// </body>
+
+// </html>`, function (err) {
+//   if (err) throw err;
+//   console.log('Saved!');
+// });
